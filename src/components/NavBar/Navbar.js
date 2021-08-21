d36f9770c564d84b39623ca7fc342172f8e9fc3a
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-20 22:51:23 -0500

    Fix import of NavBar.css to Navbar.css

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { nav, navItem, title } from './NavBar.css';
+import { nav, navItem, title } from './Navbar.css';
 import pokedex from '../../assets/images/title.png';
 
 const NavBar = ({ btn, openFilter }) => {
